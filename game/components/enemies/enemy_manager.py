c6886b578b74f96002f6b908af1a0520e6991fc9
ForeceCS <ale30deagosto@gmail.com>
2023-07-20 00:02:48 +0100

    .

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -20,7 +20,8 @@ class EnemyManager:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len(self.enemies) < 1:
+        if len(self.enemies) < 2:
             enemy = Enemy()
             self.enemies.append(enemy)
+
 
